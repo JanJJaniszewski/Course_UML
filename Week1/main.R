7c14fdae2f45f50433aeaacb8ff1d43b3e414570
Felipe Benítez <fbschlapp@gmail.com>
2022-01-10 10:55:55 +0100

    adding quadratic hinge and fixing small bugs

--- Week1/main.R
+++ Week1/main.R
@@ -2,10 +2,8 @@
 set.seed(1)
 
 #### Load packages ####
-library(pacman)
-p_load(tidyverse)
-p_load(SVMMaj)
-p_load(vtreat)
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, SVMMaj, vtreat)
 
 #### Load and prepare data ####
 load('Week1/Data/bank.RData')
@@ -18,11 +16,17 @@ bank <- bank[sample(nrow(bank), n), ]
 bank <- bank %>% select(-c(emp.var.rate, euribor3m))
 
 # Transform months to numbers
-bank$month <- factor(bank$month, levels = c("jan", "feb", 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'))
+bank$month <- factor(bank$month, levels = c("jan", "feb", 'mar', 'apr', 'may',
+                                            'jun', 'jul', 'aug', 'sep', 'oct',
+                                            'nov', 'dec'))
 bank$month <- bank$month %>% as.numeric
 
-# Transform education to numberic
-bank$education <- factor(bank$education, levels = c('illiterate', 'basic.4y', 'baic.6y', 'basic.9y', 'high.school', 'professional.course', 'university.degree'))
+# Transform education to numeric
+bank$education <- factor(bank$education, levels = c('illiterate', 'basic.4y',
+                                                    'basic.6y', 'basic.9y',
+                                                    'high.school',
+                                                    'professional.course',
+                                                    'university.degree')) # why discard unknown?
 bank$education <- bank$education %>% as.numeric
 
 # Transform data for modeling
@@ -31,7 +35,7 @@ treatment <- designTreatmentsC(bank,
                                'y',
                                'yes', 
                                codeRestriction = c('clean', 'lev'))
-bank_prepared <-  prepare(treatment, bank)   
+bank_prepared <- prepare(treatment, bank)   
 
 # Scale X and add a column with 1's to X
 X <- bank_prepared %>% select(-y) %>% scale
@@ -42,57 +46,134 @@ y <- bank_prepared %>% pull(y) %>% as.numeric
 y[y == 1] <- -1
 y[y == 2] <- 1
 
-
 #### Own SVM function ####
-# Initialize
-error <- 0.0000001 # Stopping criterion for improvement of function
-m <- ncol(X) - 1 # Number of columns (excluding the constant column)
-w <- matrix(0.1, m, 1) # Initial weights
-lambda <- 15 # Lambda (parameter)
-constant <- 0 # Initial c
-v <- t(cbind(constant, t(w))) # [c, wT]
 
-# P matrix
-P <- diag(1, m+1)
-P[1,1] <- 0
+# Initializing loss, update and majorizing parameter functions based on hinge
+quadSVMLoss <- function(y, q, lambda, w){
+  return( sum(pmax(0, 1 - y * q)^2) + lambda * crossprod(w) )
+}
 
+quadSVMParams <- function(y, q){
+  a <- 1
+  b <- ifelse(y == -1, min(q, -1), max(q, 1))
+  c <- ifelse(y == -1, 
+              ifelse(q <= -1, a - 2*(1 + q) + (1 + q)^2, 1),
+              ifelse(q > 1, a - 2*(1 - q) + (1 - q)^2, 1))
+  params <- list(a,b,c)
+  names(params) <- c("a","b","c")
+  return(params)
+}
+  
+quadSVMUpdate <- function(X, A, lambda, P, b){
+  return( solve(t(X) %*% X + lambda * P) %*% t(X) %*% b )
+}
 
-# Entering while function
-k <- 1
-l_svm <- sum(pmax(0, 1 - y * q)) + lambda * crossprod(w)
-l_svm_old <- l_svm + l_svm * error + 1
-  
-while(((l_svm_old - l_svm) / l_svm_old) > error){
-  k = k+1
-  # Assign previous run loss to old loss
-  l_svm_old <- l_svm
-  
-  # Predict q
-  q <- X %*% v
-  
-  q %>% head
-  y %>% head
-  
-  # Compute a, b, c, A using absolute hinge
+absSVMLoss <- function(y, q, lambda, w){
+  return( sum(pmax(0, 1 - y * q)) + lambda * crossprod(w) )
+}
+
+absSVMParams <- function(y, q){
   a <- pmax((4 * abs(1 - y * q)), 1e-4) ^ (-1)
   b <- y*(a + 1/4)
   c <- a + 1/2 + abs(1 - y * q)/4
-  A <- diag(x=a %>% as.vector, n, n)
-  
-  # compute the new loss
-  l_svm <- sum(pmax(0, 1 - y * q)) + lambda * crossprod(w)
-  
-  # Update v
-  v <- solve(t(X) %*% A %*% X + lambda * P, t(X) %*% b)
-  
-  constant <- v[1]
-  w <- v[2:length(v)]
-  
-  # Print update information
-  print('---------------')
-  print(k)
-  print(l_svm[1,1])
+  params <- list(a,b,c)
+  names(params) <- c("a","b","c")
+  return(params)
 }
 
-# Execute SVMMaj function on data
-svmmaj(X = X, y=y, hinge = 'absolute', lambda = lambda)
+absSVMUpdate <- function(X, A, lambda, P, b){
+  return( solve(t(X) %*% A %*% X + lambda * P, t(X) %*% b) )
+}
+
+ownSVMMAJ <- function(y, X, lambda, eps, hinge, debug = FALSE){
+  ####
+  # Purpose :
+  #   Fit an SVM with absolute or quadratic error for predicting y based on X.
+  # Inputs  : 
+  #   y           vector of 1's and (-1)'s, treatment variable
+  #   X           smth
+  #   lambda      smth
+  #   eps         smth
+  #   hinge       smth
+  #   debug       smth
+  # Returns :
+  #   constant    smth
+  #   w           smth
+  ####
+  
+  # Initializing values
+  m <- ncol(X) - 1 # Number of columns (excluding the constant column)
+  w <- matrix(0.1, m, 1) # Initial weights
+  constant <- 0 # Initial c
+  v <- t(cbind(constant, t(w))) # [c, wT]
+  P <- diag(1, m+1)
+  P[1,1] <- 0
+  q <- X %*% v
+  
+  # Defining appropriate functions based on hinge
+  if(hinge == 'absolute'){
+    SVMLoss <- absSVMLoss
+    SVMUpdate <- absSVMUpdate
+    SVMParams <- absSVMParams
+  } else { # quadratic hinge
+    SVMLoss <- quadSVMLoss
+    SVMUpdate <- quadSVMUpdate
+    SVMParams <- quadSVMParams
+  }
+  
+  # Entering while function
+  k <- 0
+  l_svm <- SVMLoss(y, q, lambda, w)
+  l_svm_old <- l_svm + l_svm * eps + 1
+  
+  while(((l_svm_old - l_svm) / l_svm_old) > eps){
+    # Update number of iterations
+    k = k+1
+    
+    # Assign previous run loss to old loss
+    l_svm_old <- l_svm
+    
+    # Predict q
+    q <- X %*% v
+    
+    # Compute a, b, c, A
+    params <- SVMParams(y,q)
+    a <- params$a
+    b <- params$b
+    c <- params$c
+    A <- diag(x = a %>% as.vector, n, n)
+    
+    # compute the new loss
+    l_svm <- SVMLoss(y, q, lambda, w)
+    
+    # Update v based on hinge
+    v <- SVMUpdate(X, A, lambda, P, b)
+    constant <- v[1]
+    w <- v[2:length(v)]
+    
+    # Print update information for debugging
+    if(debug){
+      print('---------------')
+      print(k) # number of iterations
+      print(l_svm[1,1]) # loss function value
+    }
+  }
+  
+  output <- list(v, constant)
+  names(output) <- c("v","c")
+  return(output)
+}
+
+# Initializing params
+eps <- 0.0000001 # Stopping criterion for improvement of function
+lambda <- 15 # Lambda (parameter)
+
+# Executing own SVMMaj 
+resSVMMAJabs <- ownSVMMAJ(X = X, y=y, hinge = 'absolute', lambda = lambda, eps = eps)
+resSVMMAJquad <- ownSVMMAJ(X = X, y=y, hinge = 'quadratic', lambda = lambda, eps = eps)
+
+# Executing package SVMMaj
+resPackageSVMMAJabs <- svmmaj(X = X, y=y, hinge = 'absolute', lambda = lambda)
+resPackageSVMMAJquad <- svmmaj(X = X, y=y, hinge = 'quadratic', lambda = lambda)
+
+
