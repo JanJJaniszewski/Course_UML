b3d4c4641c77e3c7dc694a8fdb51f023cdf42431
Jan Janiszewski <pole19901990@gmail.com>
2022-01-12 18:18:16 +0100

    finished unclean version of adas booster

--- Week2/main.R
+++ Week2/main.R
@@ -1,6 +1,6 @@
 #### Load packages and init ####################################################
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(tidyverse, vtreat, caret)
+pacman::p_load(tidyverse, vtreat, caret, rpart, ohenery)
 set.seed(42)
 
 
@@ -10,8 +10,6 @@ load('Week2/bookings.RData')
 
 ##### Analyze #####
 bookings %>% group_by(is_cancelled) %>% summarise_all(mean) %>% head()
-dim(bookings)
-
 
 ##### Prepare #####
 # Define train and test sets
@@ -29,10 +27,62 @@ trainSet <- prepare(treatment, trainSet)
 testSet <- prepare(treatment, testSet)
 
 # Scale X and add a column with 1's to X
-X.train <- trainSet %>% select(-is_cancelled) %>% scale
-X.test <- testSet %>% select(-is_cancelled) %>% scale
+X.train <- trainSet %>% select(-is_cancelled) %>% scale %>% as_tibble
+X.test <- testSet %>% select(-is_cancelled) %>% scale%>% as_tibble
 
 # Set y to -1 and 1 for negative and positive cases
-y.train <- trainSet %>% pull(is_cancelled) %>% as.numeric %>% `-`(1)
-y.test <- testSet %>% pull(is_cancelled) %>% as.numeric %>% `-`(1)
+y.train <- trainSet %>% pull(is_cancelled) %>% as.numeric %>% replace(.==1, -1) %>% replace(.==2, 1)
+y.test <- testSet %>% pull(is_cancelled) %>% as.numeric %>% replace(.==1, -1) %>% replace(.==2, 1)
 
+
+
+#### AdaBoost ####
+
+##### Init #####
+train_ada <- function(X, y, M=10, classifier_method='rpart', verbose=0){
+  w <- rep(1/nrow(X.train), nrow(X.train))
+  G <- list()
+  M <- seq(1, M)
+  alpha <- list()
+  
+  if(verbose > 0) print('Ada starts her training')
+  for (m in M){
+    G[[m]] <- train(x = X, y=y, method = classifier_method, weights = w)
+    preds_Gm <- sign(predict(G[[m]], X))
+    err_m <- sum(w * (y != preds_Gm)) / sum(w)
+    alpha[[m]] <- log((1 - err_m) / err_m)
+    w <-  (w * exp(alpha[[m]] * (y != preds_Gm))) %>% normalize
+    
+    if (verbose>0){
+      print('------------')
+      print(m)
+      print(alpha[[m]])
+    }
+  }
+  ada <- list()
+  ada[['alpha']] <- alpha
+  ada[['G']] <- G
+  return(ada)
+}
+
+ada <- train_ada(X.train, y.train, M=2, classifier_method = 'rpart', verbose=1)
+
+ask_ada_to_predict <- function(X, ada, verbose=0){
+  M <- ada$alpha %>% length
+  M <- seq(1, M)
+  alpha <- ada$alpha
+  G <- ada$G
+  preds <- data.frame(row.names = seq(1, nrow(X)))
+  
+  if(verbose > 0) print('Ada starts to predict')
+  for(m in M){
+    preds[m] <- alpha[[m]] * predict(G[[m]], X)
+    
+  }
+  preds_final <- apply(X = preds, MARGIN = 1, FUN = sum) %>% sign
+  
+  return(preds_final)
+}
+
+preds <- ask_ada_to_predict(X=X.test, ada=ada)
+confusionMatrix(preds %>% as.factor, y.test %>% as.factor)
